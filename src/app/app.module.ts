60776efdbca2cef3868052b52a688a01b0e13d17
DJ diox <markuswagner93@gmail.com>
2017-09-30 01:24:49 +0200

    clean up added base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
+/**
+ * Angular Core Component Imports
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/**
+ * Additional Library Imports
+ */
+
+/**
+ * Project based imports
+ */
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    CockpitComponent
   ],
   imports: [
     BrowserModule
